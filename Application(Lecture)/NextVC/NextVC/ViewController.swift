88dbd3c621408806176b606871d25ee3d162b485
pooooobi <cyh4662@icloud.com>
2022-09-21 21:52:19 +0900

    VC(feat): 간접, 직접 세그웨이

--- Application(Lecture)/NextVC/NextVC/ViewController.swift
+++ Application(Lecture)/NextVC/NextVC/ViewController.swift
@@ -39,15 +39,31 @@ class ViewController: UIViewController {
     
     // 3) 스토리보드에서의 화면 이동(간접 세그웨이)
     @IBAction func storyboardWithSegueButtonTapped(_ sender: UIButton) {
-        
-
-        
-        
-        
-        
+        performSegue(withIdentifier: "toThirdVC", sender: self)
     }
     
-
+    // 세그웨이를 통해 데이터를 전달하기 위해서는 prepate 메서드를 재정의 해야함
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toThirdVC" {
+            let thirdVC = segue.destination as! ThirdViewController
+            
+            thirdVC.modalPresentationStyle = .fullScreen
+            
+            // 데이터 전달
+            thirdVC.someString = "ThirdViewController"
+        }
+        
+        if segue.identifier == "toFourthVC" {
+            let fourthVC = segue.destination as! FourthViewController
+            
+            fourthVC.modalPresentationStyle = .fullScreen
+            fourthVC.someString = "FourthViewController"
+        }
+    }
     
+    // 직접적으로 버튼을 눌렀을 때 작동하는 세그웨이에서만 가능함
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        return true
+    }
 }
 
